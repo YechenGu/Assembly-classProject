66a2dcb7dd314e52b16579d19e3178ded560f6a7
muaXiaoNing <43727142+muaXiaoNing@users.noreply.github.com>
2019-11-02 17:04:43 +0800

    Add files via upload
    [-]fix bugs about number '0'
    [-]fix bugs about queries when previously having queries successfully 
    [+]add UI and instructions

--- Assembly.asm
+++ Assembly.asm
@@ -1,35 +1,67 @@
 ﻿.386
-DATA SEGMENT USE16
-INFO DB 200 DUP('a')
+SHOWSTR MACRO X
+        LEA DX,X
+        MOV AH,9
+        INT 21H
+        ENDM
+GETINPUT MACRO
+         MOV AH,1
+         INT 21H
+         ENDM
+CLEAR    MACRO
+         MOV AX,3
+         INT 10H 
+         ENDM
+
+DATA SEGMENT
+INFO DW 200 DUP(20H)
 STU  DW 10 DUP(0)
-WELCOME DB 0AH,0DH,'Welcome to the Student Management System'
-        DB 0AH,0DH,'Choose 1 to input , 2 to search , q to quit'
+WELCOME DB 0AH,0DH,' ===================================='
+        DB 0AH,0DH,'||            Main  Menu            ||'
+        DB 0AH,0DH,'||                                  ||'
+        DB 0AH,0DH,'||       Press  1  to  input        ||'
+        DB 0AH,0DH,'||       Press  2  to  query        ||'
+        DB 0AH,0DH,'||       Press  q  to  quit         ||'
+        DB 0AH,0DH,' ===================================='
         DB 0AH,0DH,'$'
-SCR1 DB 0AH,0DH,'Please input by the order:Number     Score     Rank'
-     DB 0AH,0DH,'$'
-SCR2 DB 0AH,0DH,'Number      Score      Rank'
-     DB 0AH,0DH,'$'
+SCR1    DB 0AH,0DH,' ================================================'
+        DB 0AH,0DH,'||             Input   Instructions             ||'
+        DB 0AH,0DH,'||                                              ||'
+        DB 0AH,0DH,'||      Input by the order "Id Grade Rank "     ||'
+        DB 0AH,0DH,'||      Press SPACE to confirm every input      ||'
+        DB 0AH,0DH,'||    Press ENTER when completing all inputs    ||'
+        DB 0AH,0DH,' ================================================'
+        DB 0AH,0DH,'$'
+SCR2    DB 0AH,0DH,' ==============================================='
+        DB 0AH,0DH,'||             Query   Instructions            ||'
+        DB 0AH,0DH,'||                                             ||'
+        DB 0AH,0DH,'||       Press SPACE to excute every query     ||'
+        DB 0AH,0DH,'||   Press ENTER when completing all queries   ||'
+        DB 0AH,0DH,' ==============================================='
+        DB 0AH,0DH,'Id         Grade        Rank'
+        DB 0AH,0DH,'$'
+GAP     DB '        $'
 FULL_INPUT DB 0AH,0DH,'You  have  completed  30  inputs'
            DB 0AH,0DH,'$'
-QUERY_FAIL DB 0AH,0DH,'NOT   FOUND'
+QUERY_FAIL DB 20H,'       NOT         FOUND'
            DB 0AH,0DH,'$'
 DATA ENDS
-STACK SEGMENT USE16 STACK
-    DB 200 DUP(0)
+
+STACK SEGMENT STACK
+    DB 100 DUP(0)
 STACK ENDS
-CODE SEGMENT USE16
+
+CODE SEGMENT
 ASSUME CS:CODE,DS:DATA,SS:STACK
 START: MOV AX,DATA
-       MOV DS,AX    
-       LEA DX,WELCOME   ;显示字符串
-       MOV AH,9
-       INT 21H
-       MOV AH,1      ;读取输入
-       INT 21H
+       MOV DS,AX
+       CLEAR   
+       SHOWSTR WELCOME
+       GETINPUT
        CMP AL,'q'
-       JE EXIT
+       JE JEXIT
        CMP AL,'Q'
-       JE EXIT
+       JE JEXIT
        AND AL,0FH
        CMP AL,1
        JE D1
@@ -43,18 +75,20 @@ D1:    CALL R1       ;跳转到输入成绩
 D2:    CALL R2       ;跳转到查询成绩
        JMP START 
 
+JEXIT:  JMP EXIT
+
 R1 PROC              
-       LEA DX,SCR1   
-       MOV AH,9
-       INT 21H       ;显示字符串
+       CLEAR 
+       SHOWSTR SCR1
        LEA SI,STU
        LEA BP,INFO
        MOV CX,10     ;设定寄存器,CX用来计算成绩和名次有几个数字
-       MOV DI,0      ;DI用来计数
-    LOPA1:MOV AX,0 
-          MOV [SI],AX
-    LOPA:  MOV AH,1
-       INT 21H
+       MOV DI,0      ;DI用来计算数据是学号还是成绩和名次
+  LOPA1:
+       MOV AX,0 
+       MOV [SI],AX   ;SI用来传递结果
+  LOPR1:  
+       GETINPUT
        CMP AL,'q'
        JE EXIT
        CMP AL,'Q'
@@ -72,10 +106,10 @@ R1 PROC
        PUSH AX
        MOV AX,DI
        MOV BL,3
-       DIV BL
+       DIV BL               ;判断输入的数据是学号还是成绩和名次
        CMP AH,0
-       JNE EXEC2
-       ;处理数据，用到了AX,BX,DX,SI
+       JNE DO_GR
+       ;处理学号，用到了AX,BX,DX,SI。原理：将SI里面的数乘10再加上AX里面的数
        POP AX
        MOV BX,[SI]
        ADD BX,BX
@@ -85,25 +119,30 @@ R1 PROC
        ADD BX,DX
        MOV [SI],BX
        ADD [SI],AX
-       ;处理数据完成
-       JMP LOPA
-EXEC2: POP AX
-       MOV [BP],AX
-       INC BP
+       ;处理学号完成
+       JMP LOPR1
+   DO_GR: 
+       ;处理成绩和名次
+       POP AX
+       MOV DS:[BP],AX             ;BP前面必须加上DS，否则默认存到SS之中
+       ADD BP,2
        DEC CX
-       JMP LOPA
-       ;处理循环体
+       JMP LOPR1
+       ;处理成绩和名次完成
        RET
 R1 ENDP
 
+EXIT:  MOV AH,4CH
+       INT 21H
+
+JSTART: JMP START
+
 R2 PROC    
-       LEA DX,SCR2   ;显示字符串
-       MOV AH,9
-       INT 21H
-       ;设定寄存器
-       MOV DI,0;存放输入的数据
- LOPB: MOV AH,1
-       INT 21H
+       CLEAR 
+       SHOWSTR SCR2
+       MOV DI,0             ;用来存放输入的数据
+   LOPR2:
+       GETINPUT
        CMP AL,'q'
        JE EXIT
        CMP AL,'Q'
@@ -111,10 +150,10 @@ R2 PROC
        CMP AL,20H
        JE QUERY             ;确保输入不是空格  
        CMP AL,0DH              
-       JE START             ;确保输入不是回车
+       JE JSTART             ;确保输入不是回车
        AND AL,0FH
        MOV AH,0
-       ;处理数据，用到了AX,BX,DX,DI
+       ;处理数据，用到了AX,BX,DX,DI，原理和处理输入的学号相同
        MOV BX,DI
        ADD BX,BX
        MOV DX,BX
@@ -124,82 +163,89 @@ R2 PROC
        MOV DI,BX
        ADD DI,AX
        ;处理数据完成
-       JMP LOPB
-       ;处理循环体  
+       JMP LOPR2
        RET
 R2 ENDP
 
-SPACE: MOV AX,DI
+
+
+SPACE: MOV AX,DI            ;DI是输入的结果
        MOV BL,3
        DIV BL
-       CMP AH,0
-       JE  SP1
- LOP3: INC BP
-       LOOP LOP3
+       CMP AH,0             ;判断输入的是学号还是成绩和名次
+       JE  DO_ID
+   LOPGR:                    ;如果输入的是成绩和名次 
+       ADD BP,2
+       LOOP LOPGR
        INC DI
        MOV CX,10
        CMP DI,30
        JE  WARNING
-       JMP LOPA  
-SP1:   ADD SI,2
+       JMP LOPR1  
+   DO_ID:                     ;如果输入的是学号   
+       ADD SI,2
        INC DI
        JMP LOPA1
-  WARNING: LEA DX,FULL_INPUT   ;显示字符串
-             MOV AH,9
-             INT 21H
-             JMP START
+   WARNING:   
+       SHOWSTR FULL_INPUT
+       JMP START
 
 QUERY: MOV CX,10
        LEA SI,STU
        MOV AX,0
-  Q:   CMP DI,[SI]
+    LOPQ: 
+       CMP DI,[SI]
        JE  QSUCCESS
        ADD SI,2
        INC AL
-       LOOP Q
+       LOOP LOPQ
        JMP QFAIL
 
-QSUCCESS:LEA BP,INFO
-         MOV AH,20
+QSUCCESS:SHOWSTR GAP
+         LEA BP,INFO
+         MOV AH,40
          MUL AH
          ADD BP,AX
          MOV CX,10
-  LOP4:  MOV AL,[BP]
-         CMP AL,0    ;不知道为什么要和0比较
-         JE FIN_OUP
+    LOP_GRADE:  
+         MOV AL,DS:[BP]
+         CMP AL,20H    
+         JE FIN_GRADE
          ADD AL,48 
          MOV DL,AL
          MOV AH,2
          INT 21H
-         INC BP
+         ADD BP,2
          DEC CX
-         JMP LOP4
- FIN_OUP:MOV DL,32
-         MOV AH,2
-         INT 21H
+         JMP LOP_GRADE
+    FIN_GRADE:
+         SHOWSTR GAP
          ADD BP,CX
+	  ADD BP,CX
          MOV CX,0
-  LOP5:  MOV AL,[BP];
-         CMP AL,0
-         JE FIN_OUT 
+    LOP_RANK:  
+         MOV AL,DS:[BP]
+         CMP AL,20H
+         JE FIN_RANK 
          ADD AL,48 
          MOV DL,AL
          MOV AH,2
          INT 21H
-         INC BP
-         JMP LOP5
- FIN_OUT:MOV DL,10
+         ADD BP,2
+         JMP LOP_RANK
+    FIN_RANK:
+         MOV DL,10
          MOV AH,2
          INT 21H
-         JMP LOPB                
+         MOV DL,13
+         MOV AH,2
+         INT 21H
+	  MOV DI,0
+         JMP LOPR2                
 
-QFAIL: LEA DX,QUERY_FAIL   ;显示字符串
-       MOV AH,9
-       INT 21H
+QFAIL: SHOWSTR QUERY_FAIL
        MOV DI,0
-       JMP LOPB 
+       JMP LOPR2 
 
-EXIT:  MOV AH,4CH
-       INT 21H
 CODE ENDS
 END START
